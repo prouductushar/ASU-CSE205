0fb7db8ad0b18c53d160a86f24e9399c35258b9d
Tushar Tyagi <tushar.tyagi.dxb@gmail.com>
2022-05-19 13:12:47 +0400

    Create MyListIterator.java

---
+++ #6/MyLinkedList/MyListIterator.java
@@ -0,0 +1,22 @@
+/**
+A list iterator allows access of a position in a list.
+This interface contains a subset of the methods of the
+standard java.util.ListIterator interface. The methods for
+backward traversal are not included.
+*/
+public interface MyListIterator
+{
+	/**
+	   Moves the iterator past the next element.
+	   @return the traversed element
+	*/
+	Object next();
+	
+	/**
+	   Tests if there is an element after the iterator
+	   position.
+	   @return true if there is an element after the iterator
+	   position
+	*/
+	boolean hasNext();
+}
